934cdbd36db39d42abd7fd08a89e82643d3c39a7
Marko-Buda <44259192+Marko-Buda@users.noreply.github.com>
2021-12-14 01:54:42 +0100

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -2,5 +2,5 @@
 
 #git clone git@github.com:Marko-Buda/Course2-AgileDevelopmentwithAzure.git
 #cd Course2-AgileDevelopmentwithAzure/
-az webapp up -n demo_project
+az webapp up -n demo-project
 #az webapp config set -g Course2-Agile-Development-with-Azure -n project2_CI_and_CD
